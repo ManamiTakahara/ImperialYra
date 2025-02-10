6dfceedc6501c9b24d66b5630969d2c41f595a05
ManamiTakahara <uni202412_09@telework.to>
2025-02-10 14:39:44 +0900

    クラスImperialYraに基本クラスを作成

--- ImperialYra/YearNumber.cs
+++ ImperialYra/YearNumber.cs
@@ -8,5 +8,16 @@ namespace ImperialYra
 {
     internal class YearNumber
     {
+        //基本クラス
+        private int year1;
+        private int year2;
+        public int Year1 { get; set; }
+        public int Year2 { get; set; }
+
+        public YearNumber(int year1, int year2)
+        {
+            this.year1 = year1;
+            this.year2 = year2;
+        }
     }
 }
