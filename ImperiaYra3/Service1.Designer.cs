c7abb76dce2a0ca9fc619880e516a4d03325015b
ManamiTakahara <uni202412_09@telework.to>
2025-02-24 12:05:39 +0900

    別のコードでアプローチ

---
+++ ImperiaYra3/Service1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace ImperiaYra3
+{
+    partial class Service1
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Service1
+            // 
+            this.ServiceName = "Service1";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ImperialYra3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ImperialYra3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.westernLabel = new System.Windows.Forms.Label();
+            this.inperiaLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(401, 50);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(150, 65);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "検索";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(401, 190);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(147, 67);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "リセット";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // westernLabel
+            // 
+            this.westernLabel.AutoSize = true;
+            this.westernLabel.Location = new System.Drawing.Point(38, 144);
+            this.westernLabel.Name = "westernLabel";
+            this.westernLabel.Size = new System.Drawing.Size(152, 18);
+            this.westernLabel.TabIndex = 3;
+            this.westernLabel.Text = "西暦が表示されます";
+            // 
+            // inperiaLabel
+            // 
+            this.inperiaLabel.AutoSize = true;
+            this.inperiaLabel.Location = new System.Drawing.Point(38, 214);
+            this.inperiaLabel.Name = "inperiaLabel";
+            this.inperiaLabel.Size = new System.Drawing.Size(152, 18);
+            this.inperiaLabel.TabIndex = 4;
+            this.inperiaLabel.Text = "和暦が表示されます";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 298);
+            this.Controls.Add(this.inperiaLabel);
+            this.Controls.Add(this.westernLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.searchButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label westernLabel;
+        private System.Windows.Forms.Label inperiaLabel;
+    }
+}
+
